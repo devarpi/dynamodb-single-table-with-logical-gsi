274bdcaea77a877526a6811b5e5d5a5588d20fee
Devarpi Sheth <110236879+devarpi@users.noreply.github.com>
2024-03-08 19:01:53 -0500

    Add files via upload

---
+++ test/dynamodb-single-table-with-logical-gsi.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as DynamodbSingleTableWithLogicalGsi from '../lib/dynamodb-single-table-with-logical-gsi-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/dynamodb-single-table-with-logical-gsi-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new DynamodbSingleTableWithLogicalGsi.DynamodbSingleTableWithLogicalGsiStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
